a6376cdc67a2ffb85ec4f5b08c1e7380733c76fa
Sayonnara Seixas <95715855+Sayonnara@users.noreply.github.com>
2025-04-25 00:22:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+[] Descobrir quando o usuário  clicou no botão
+[] Assim que o botão  for clicado
+[] Trocar a cor de fundo
+[] Trocar a iamgem principal
+
+ */
+
+let circulo =document.querySelector(".circulo")
+
+
+function trocarCor(cor){
+    circulo.style.background =cor;
+}
